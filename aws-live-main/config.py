9598566d905c33e082a689a433d7f04d5fd42255
jason6356 <70106185+jason6356@users.noreply.github.com>
2023-08-24 15:31:57 +0800

    Delete aws-live-main directory

--- aws-live-main/config.py
+++
@@ -1,6 +0,0 @@
-customhost = "database-1.cgj7jfy75lmm.us-east-1.rds.amazonaws.com"
-customuser = "aws_user"
-custompass = "Bait3273"
-customdb = "employee"
-custombucket = "lohkangsheng-employee"
-customregion = "us-east-1"
